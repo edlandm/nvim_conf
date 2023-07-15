b502b949c08b16f811db79ff5a1302ad475a311a
Miles Edland <miles.edland@gmail.com>
2023-07-15 13:54:05 -0700

    Replace treesitter.rainbow module with rainbow-delimiters plugin
    Treesitter modules are going to be depricated. rainbow-delimiters is the
    official port. This is just getting ahead of things before my rainbows break

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -77,7 +77,6 @@ return {
         },
       }
     },
-    'p00f/nvim-ts-rainbow',
   },
   opts = {
     highlight = {
@@ -85,13 +84,6 @@ return {
     },
     indent = {
       enable = true
-    },
-    rainbow = {
-      enable = true,
-      -- Also highlight non-bracket delimiters like html tags, boolean or
-      -- table: lang -> boolean
-      extended_mode = true,
-      max_file_lines = 5000, -- Do not enable for files with more than n lines, int
     },
     ensure_installed = {
       "bash",
