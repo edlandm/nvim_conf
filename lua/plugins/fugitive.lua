84d3f71ff89d7aa842590a209b9d7acad8486cea
Miles Edland <miles.edland@gmail.com>
2023-07-18 21:16:41 -0700

    plugins/fugitive.lua - add <leader>gn mapping to view commits from current branch

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,8 +1,13 @@
 return {
   'tpope/vim-fugitive',
-  lazy = false,
   keys = {
     { '<leader>gs', '<cmd>Git<cr>', desc = 'Git status' },
     { '<leader>gd', '<cmd>Gvdiffsplit!<cr>', desc = 'Git diff' },
-  }
+    -- TODO: allow user to provide the branch to check against
+    { '<leader>gn', '<cmd>Git log --oneline --cherry develop..HEAD<cr>', desc = 'List commits from the current branch' },
+  },
+  cmd = {
+    "Git",
+    "Gvdiffsplit",
+  },
 }
