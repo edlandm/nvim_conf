c1e7758eb47d9f43d36aac94dada466ca992869c
Miles Edland <miles.edland@gmail.com>
2023-07-19 23:29:46 -0700

    add plugins/carbon-now.lua

---
+++ lua/plugins/carbon-now.lua
@@ -0,0 +1,16 @@
+return {
+  "ellisonleao/carbon-now.nvim",
+  cmd = "CarbonNow",
+  opts = {
+    open_cmd = "click",
+    options = {
+      drop_shadow = true,
+      font_family = "Ubuntu+Mono",
+      line_number = true,
+      theme = "material",
+      titlebar = "",
+      watermark = false,
+      window_theme = "none",
+    },
+  }
+}
