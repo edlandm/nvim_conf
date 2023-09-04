a1a4462b18bfcec56102c943222e120111a86650
Miles Edland <miles.edland@gmail.com>
2023-09-03 18:41:28 -0700

    init.lua - remove sql from Conjure filetype list

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,5 +2,22 @@
 -- rest of the configuration can be in Fennel
 return {
     {"Olical/aniseed", lazy=false},
-    {"Olical/conjure", lazy=false},
+    {
+      "Olical/conjure",
+      lazy=false,
+      init = function()
+      vim.g["conjure#filetypes"] = {
+        "clojure",
+        "fennel",
+        "janet",
+        "lisp",
+        "lua",
+        "python",
+        "racket",
+        "rust",
+        "scheme",
+        -- "sql",
+      }
+      end,
+    },
   }
