fecd722ee439a264363ba1a202e8ec0ccb833136
Miles Edland <miles.edland@gmail.com>
2023-10-10 11:43:49 -0700

    plugins/lspconfig.lua - add c# configuration

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,7 +2,8 @@ return {
   'neovim/nvim-lspconfig',
   event = "VeryLazy",
   config = function()
-    require('lspconfig').rust_analyzer.setup({
+    local lspconfig = require('lspconfig')
+    lspconfig.rust_analyzer.setup({
       settings = {
         ['rust-analyzer'] = {
           diagnostics = {
@@ -11,9 +12,20 @@ return {
         },
       },
     })
+    lspconfig.csharp_ls.setup({
+      root_dir = function(startpath)
+        return lspconfig.util.root_pattern("*.sln")(startpath)
+          or lspconfig.util.root_pattern("*.csproj")(startpath)
+          or lspconfig.util.root_pattern("*.fsproj")(startpath)
+          or lspconfig.util.root_pattern(".git")(startpath)
+      end,
+      on_attach = on_attach,
+      capabilities = capabilities
+    })
   end,
   ft = {
-    'rust',
+      'rust',
+      'cs',
   },
   dependencies = {
     'simrat39/rust-tools.nvim',
