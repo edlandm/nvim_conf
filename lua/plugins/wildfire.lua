d50d826825ff189ca16e6695a1990302962c621b
Miles Edland <miles.edland@gmail.com>
2023-10-28 08:54:01 -0700

    add plugins/wildfire

---
+++ lua/plugins/wildfire.lua
@@ -0,0 +1,21 @@
+return {
+    "sustech-data/wildfire.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+        require("wildfire").setup({
+          surrounds = {
+              { "(", ")" },
+              { "{", "}" },
+              { "<", ">" },
+              { "[", "]" },
+          },
+          keymaps = {
+              init_selection = "<CR>",
+              node_incremental = "<CR>",
+              node_decremental = "<BS>",
+          },
+          filetype_exclude = { "qf" }, --keymaps will be unset in excluding filetypes
+      })
+    end,
+}
