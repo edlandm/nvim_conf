922176d614fcc625b0f12d6e197738b91c91c5be
Miles Edland <miles.edland@gmail.com>
2024-02-11 09:18:08 -0800

    add plugins/executor.lua

---
+++ lua/plugins/executor.lua
@@ -0,0 +1,17 @@
+return {
+  "google/executor.nvim",
+  config = true,
+  cmd = {
+    "ExecutorRun",
+    "ExecutorSetCommand",
+    "ExecutorShowDetail",
+    "ExecutorHideDetail",
+    "ExecutorToggleDetail",
+    "ExecutorSwapToSplit",
+    "ExecutorSwapToPopup",
+    "ExecutorShowPresets",
+    "ExecutorShowHistory",
+    "ExecutorReset",
+    "ExecutorOneOff",
+  }
+}
