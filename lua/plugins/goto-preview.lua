591dccd7fe08f1e2516afc67abb1947f5086d1eb
Miles Edland <miles.edland@gmail.com>
2024-03-05 16:47:14 -0800

    plugins/goto-preview.lua - add `g=` mapping to preview declaration

--- lua/plugins/goto-preview.lua
+++ lua/plugins/goto-preview.lua
@@ -2,6 +2,7 @@ return {
   'rmagatti/goto-preview',
   opts = {},
   keys = {
+    {'g=', "<cmd>lua require('goto-preview').goto_preview_declaration()<CR>", 'n', desc = "goto declaration"},
     {'gd', "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", 'n', desc = "goto definition"},
     {'gt', "<cmd>lua require('goto-preview').goto_preview_type_definition()<CR>", 'n', desc = "goto type definition"},
   },
