b3ed7a6c0fac929346f192150b7f11f9866e6418
Miles Edland <miles.edland@gmail.com>
2024-03-06 16:09:41 -0800

    plugins/luasnip.lua - use vscode loader to have VSCode-style snippets

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,4 +1,11 @@
 return {
   'L3MON4D3/LuaSnip',
+  event = "InsertEnter",
   build = "make install_jsregexp",
+  config = function()
+    local luasnip = require('luasnip')
+    luasnip.config.setup()
+    local snippets_dir = vim.fn.stdpath("config") .. "/snippets"
+    require("luasnip.loaders.from_vscode").lazy_load({paths = snippets_dir})
+  end,
 }
