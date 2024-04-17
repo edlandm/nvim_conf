eb526592f720fb6b85cf141a576af9abd6ea7552
Miles Edland <miles.edland@gmail.com>
2024-04-17 15:32:19 -0700

    plugins/codeium.lua - unpin commit and enable chat

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,14 +1,12 @@
 return {
   'jcdickinson/codeium.nvim',
   lazy = false,
-  -- pinning to this commit because neovim hasn't implemented
-  -- vim.fn.inputsecret yet and it's causing an error when authenticating
-  commit = "b1ff0d6c993e3d87a4362d2ccd6c660f7444599f",
   dependencies = {
     'nvim-lua/plenary.nvim',
     'hrsh7th/nvim-cmp',
   },
   opts = {
-    config_path = vim.fn.expand("~/.local/codeium.conf")
+    config_path = vim.fn.expand("~/.local/codeium.conf"),
+    enable_chat = true,
   },
 }
