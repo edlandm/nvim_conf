83023aa4df36d3d80d37fa0671fa8cb25d966e5c
Miles Edland <miles.edland@gmail.com>
2024-07-07 21:29:08 -0700

    ftplugin/cs.lua - remove custom find_cw_references function+mapping

--- ftplugin/cs.lua
+++ ftplugin/cs.lua
@@ -3,14 +3,6 @@ vim.o.foldmethod = "indent"
 vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
 
-local find_cw_references = function() -- {{{ search project for cword
-  local cword = vim.fn.expand("<cword>")
-  -- TODO: create function to find project/solution root and use that as basis
-  -- for search glob
-  vim.cmd.vimgrep({"/" .. cword .. "/j", "**/*.cs" })
-  vim.cmd.copen()
-end -- }}}
-
 -- mappings
 local mapopts = function(desc, opts) -- {{{ shorthand for adding the description
   local _t = {buffer = true, noremap = true, desc = desc}
@@ -24,5 +16,4 @@ end -- }}}
 
 -- {{{ NORMAL
 -- vim.keymap.set("n", "", "", mapopts(""))
-vim.keymap.set("n", "<localleader>/", find_cw_references, mapopts("search project for <cword> (results in quickfix)"))
 -- }}}
