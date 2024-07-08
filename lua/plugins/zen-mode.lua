f271bc9c118f3532eee83951999086ff83e24b86
Miles Edland <miles.edland@gmail.com>
2024-07-07 21:26:48 -0700

    plugins/zen-mode.lua - tweak default settings

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -4,6 +4,17 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
+    window = {
+      height = 0.97,
+      width = 0.97,
+    },
+    plugins = {
+      options = {
+        ruler = true,
+        showcmd = true,
+        laststatus = 3,
+      },
+    },
   },
   cmd = "ZenMode",
   keys = {
