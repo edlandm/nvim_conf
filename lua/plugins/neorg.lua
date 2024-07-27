b0a2b179478c47066791e7904122052b35031c85
Miles Edland <miles.edland@gmail.com>
2024-07-26 22:43:07 -0700

    lua/plugins/neorg.lua - remove default keybindings after update

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -27,34 +27,7 @@ return {
       -- ['core.ui.calendar'] = {},
       ['core.keybinds'] = {
         config = {
-          hook = function(keybinds)
-            keybinds.unmap("all", "n", "gO") -- originally mapped to open table-of-contents
-            keybinds.unmap("all", "n", "go") -- originally mapped to open table-of-contents
-
-            keybinds.remap_key("norg", "i", "<M-CR>", ";n",
-              { desc = "start new list entry" })
-
-            keybinds.map("norg", "n", "<localleader>ec",
-              "<cmd>Neorg keybind all core.looking-glass.magnify-code-block<cr>",
-              { desc = "edit code-block in new buffer"})
-
-            keybinds.map("norg", "n", "<localleader><leader>", "<cmd>Neorg toc qflist<cr>",
-              { desc = "open table-of-contents in quickfix list"})
-
-            keybinds.remap("norg", "n", "<localleader><cr>",
-              "<cmd>Neorg keybind all core.esupports.hop.hop-link vsplit<cr>",
-              { desc = "Jump to Link (Vertical Split)" })
-
-            keybinds.map("norg", "n", "<localleader>tc", function()
-              local cc = vim.wo.concealcursor
-              if cc == "" then
-                vim.wo.concealcursor = "nc"
-              else
-                vim.wo.concealcursor = ""
-              end
-              end,
-              { desc = "toggle" })
-          end,
+          default_keybinds = false,
         }
       },
       ['core.journal'] = {
