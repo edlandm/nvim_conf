088d883c2358086c0aa088112cb468f65c2be97d
Miles Edland <miles.edland@gmail.com>
2024-07-26 22:43:56 -0700

    plugins/luasnip.lua - enable lua plugins

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -3,9 +3,10 @@ return {
   event = "InsertEnter",
   build = "make install_jsregexp",
   config = function()
-    local luasnip = require('luasnip')
-    luasnip.config.setup()
+    local ls = require('luasnip')
+    ls.config.setup()
     local snippets_dir = vim.fn.stdpath("config") .. "/snippets"
     require("luasnip.loaders.from_vscode").lazy_load({paths = snippets_dir})
+    require("luasnip.loaders.from_lua").lazy_load({paths = snippets_dir})
   end,
 }
