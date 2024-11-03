cdb29ad2204d4120ea108f99739839442e189c7a
Miles Edland <miles.edland@gmail.com>
2024-11-03 09:27:32 -0800

    plugins/duck.lua - add descriptions to mappings

--- lua/plugins/duck.lua
+++ lua/plugins/duck.lua
@@ -2,7 +2,7 @@ return {
   "tamton-aquib/duck.nvim",
   event = "VimEnter",
   config = function()
-    vim.keymap.set('n', '<leader>dd', function() require("duck").hatch() end, {})
-    vim.keymap.set('n', '<leader>dx', function() require("duck").cook_all() end, {})
+    vim.keymap.set('n', '<leader>dd', function() require("duck").hatch() end, { desc = 'definitally not a duck' })
+    vim.keymap.set('n', '<leader>dx', function() require("duck").cook_all() end, { desc = 'duck season' })
   end
 }
