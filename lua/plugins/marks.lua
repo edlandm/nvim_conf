bb0c59e55ba1f7da59a27f02a98ace42a051348d
Miles Edland <miles.edland@gmail.com>
2024-11-03 09:32:09 -0800

    plugins/marks.lua - add excluded_buftypes

--- lua/plugins/marks.lua
+++ lua/plugins/marks.lua
@@ -3,6 +3,7 @@ return {
   lazy = false,
   opts = {
     builtin_marks = { ".", "<", ">", "^" },
+    excluded_buftypes = { 'nofile', 'terminal', 'quickfix', 'prompt', },
     refresh_interval = 250,
     sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
   },
