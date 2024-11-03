8aba16875f8e73375ae8ee25559de06158703a8f
Miles Edland <miles.edland@gmail.com>
2024-11-03 09:39:51 -0800

    add plugins/luminate.lua

---
+++ lua/plugins/luminate.lua
@@ -0,0 +1,22 @@
+return {
+  "mei28/luminate.nvim",
+  event = "VeryLazy",
+  config = function ()
+    local visual   = assert(vim.api.nvim_get_hl(0, {name = "Visual"}), "Error getting visual hl group")
+    local diff_add = assert(vim.api.nvim_get_hl(0, {name = "DiffAdd"}), "Error getting DiffAdd hl group")
+    require('luminate').setup({
+      duration = 200,
+      yank = {
+      },
+      paste = {
+        map = { p = 'p', P = 'P' },
+      },
+      undo = {
+        guibg = visual.bg,
+      },
+      redo = {
+        guibg = diff_add.bg,
+      }
+    })
+  end,
+}
