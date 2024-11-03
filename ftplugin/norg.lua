31e60610b314a57b20baffbd4990ae652eb47250
Miles Edland <miles.edland@gmail.com>
2024-11-03 09:24:04 -0800

    ftplugin/norg.lua - tweak todo mappings

--- ftplugin/norg.lua
+++ ftplugin/norg.lua
@@ -25,19 +25,11 @@ vim.keymap.set("n", "<localleader>lt", "<Plug>(neorg.pivot.list.toggle)", { desc
 vim.keymap.set("n", "<localleader>ta", "<Plug>(neorg.qol.todo-items.todo.task-ambiguous)", { desc = "neorg: set task to ambiguous", buffer = true })
 vim.keymap.set("n", "<localleader>tc", "<Plug>(neorg.qol.todo-items.todo.task-cancelled)", { desc = "neorg: set task to cancelled", buffer = true })
 vim.keymap.set("n", "<localleader>td", "<Plug>(neorg.qol.todo-items.todo.task-done)",      { desc = "neorg: set task to done",      buffer = true })
-vim.keymap.set("n", "<localleader>th", "<Plug>(neorg.qol.todo-items.todo.task-hold)",      { desc = "neorg: set task to hold",      buffer = true })
+vim.keymap.set("n", "<localleader>th", "<Plug>(neorg.qol.todo-items.todo.task-on-hold)",      { desc = "neorg: set task to hold",      buffer = true })
 vim.keymap.set("n", "<localleader>ti", "<Plug>(neorg.qol.todo-items.todo.task-important)", { desc = "neorg: set task to important", buffer = true })
 vim.keymap.set("n", "<localleader>tp", "<Plug>(neorg.qol.todo-items.todo.task-pending)",   { desc = "neorg: set task to pending",   buffer = true })
-vim.keymap.set("n", "<localleader>tu", "<Plug>(neorg.qol.todo-items.todo.task-recurring)", { desc = "neorg: set task to recurring", buffer = true })
+vim.keymap.set("n", "<localleader>tr", "<Plug>(neorg.qol.todo-items.todo.task-recurring)", { desc = "neorg: set task to recurring", buffer = true })
 vim.keymap.set("n", "<localleader>tu", "<Plug>(neorg.qol.todo-items.todo.task-undone)",    { desc = "neorg: set task to undone",    buffer = true })
-
-vim.keymap.set("n", "<leader>tc", function()
-  local cc = "nc"
-  if vim.wo.concealcursor == cc then
-    cc = ""
-  end
-  vim.wo.concealcursor = cc
-end, { desc = "toggle concealcursor", buffer = true })
 
 -- vim.keymap.set("n", "<localleader>", "<Plug>(neorg.)", { desc = "neorg: ", buffer = true })
 -- vim.keymap.set("n", "", "<Plug>(neorg.)", { desc = "neorg: ", buffer = true })
