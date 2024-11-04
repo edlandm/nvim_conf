e51be44bbc540a9456c848b4eccee193f72bef0f
Miles Edland <miles.edland@gmail.com>
2024-11-04 06:57:43 -0800

    add lua/neovide_settings.lua

---
+++ lua/neovide_settings.lua
@@ -0,0 +1,13 @@
+require('settings').setopts('g', {
+  { 'neovide_transparency',                  1 },
+  { 'neovide_floating_blur_amount_x',        2.0 },
+  { 'neovide_floating_blur_amount_y',        2.0 },
+  { 'neovide_floating_shadow',               true },
+  { 'neovide_floating_z_height',             10 },
+  { 'neovide_light_angle_degrees',           45 },
+  { 'neovide_light_radius',                  5 },
+  { 'neovide_hide_mouse_when_typing',        true },
+  { 'neovide_cursor_animate_in_insert_mode', true },
+  { 'neovide_cursor_vfx_mode',               'pixiedust' },
+  { 'neovide_cursor_vfx_particle_density',   10.0 },
+})
