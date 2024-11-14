8458c8c0c23cd14e6ab09157b115d552d566ace3
Miles Edland <miles.edland@gmail.com>
2024-11-14 09:10:53 -0800

    ftplugin/lua.lua - add <F5> plugin to run current file

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,3 +1,9 @@
 vim.bo.shiftwidth = 2
 vim.bo.tabstop    = 2
 vim.wo.foldmethod = "indent"
+
+vim.bo.makeprg = 'lua'
+
+require('mappings').nmap({
+  { 'execute current file with :make', '<F5>', '<cmd>make %<cr>'  },
+}, true)
