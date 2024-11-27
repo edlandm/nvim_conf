53465328345ce70436d7ff22380fb21ba3788864
Miles Edland <miles.edland@gmail.com>
2024-11-27 15:01:51 -0800

    add plugins/coerce.lua

---
+++ lua/plugins/coerce.lua
@@ -0,0 +1,9 @@
+return {
+  'gregorias/coerce.nvim',
+  event = 'VeryLazy',
+  tag = 'v4.0.0',
+  dependencies = {
+    'gregorias/coop.nvim',
+  },
+  config = true,
+}
--- lua/plugins/vim-abolish.lua
+++ lua/plugins/vim-abolish.lua
@@ -3,6 +3,10 @@ return {
   'tpope/vim-abolish',
   lazy = false,
   ft = {'gitcommit', 'markdown', 'text', 'norg'},
+  init = function ()
+    -- I decided to use gregorias/coerce.nvim for the coerce functionality
+    vim.g.abolish_no_mappings = true
+  end,
   config = function()
     -- abbreviations go here
     vim.cmd({ cmd = "Abolish", args = { "deploymenst", "deployments" } })
