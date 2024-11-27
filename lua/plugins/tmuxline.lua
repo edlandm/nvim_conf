2f2fa56cef7a8127c6f05587200bbb53323543bc
Miles Edland <miles.edland@gmail.com>
2024-11-27 15:14:08 -0800

    plugins/tmuxline.lua - only load if in tmux session

--- lua/plugins/tmuxline.lua
+++ lua/plugins/tmuxline.lua
@@ -1,5 +1,12 @@
 return {
   'edkolev/tmuxline.vim',
+	event = 'VeryLazy',
+	cond = function ()
+		if os.getenv('TMUX') then
+			return true
+		end
+		return false
+	end,
   cmd = {
     'Tmuxline',
     'TmuxlineSnapshot',
