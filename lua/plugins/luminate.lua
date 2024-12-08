3b453e48cbbee09e437607a3a4c18be92a5fc0d8
Miles Edland <miles.edland@gmail.com>
2024-12-08 09:36:58 -0800

    plugins/luminate.lua - slightly decrease highlight duration

--- lua/plugins/luminate.lua
+++ lua/plugins/luminate.lua
@@ -5,7 +5,7 @@ return {
     local visual   = assert(vim.api.nvim_get_hl(0, {name = "Visual"}), "Error getting visual hl group")
     local diff_add = assert(vim.api.nvim_get_hl(0, {name = "DiffAdd"}), "Error getting DiffAdd hl group")
     require('luminate').setup({
-      duration = 200,
+      duration = 180,
       yank = {
       },
       paste = {
