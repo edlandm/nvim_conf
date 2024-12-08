570bd14456e3d21af1006a23396c45101d880d6a
Miles Edland <miles.edland@gmail.com>
2024-12-08 09:32:16 -0800

    neorg - read workspaces from file

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,3 +1,38 @@
+---@alias path string
+
+---read the file at `path` and return a dictionary full of paths
+---@param path path
+---@return { [string]: path }
+local function read_workspaces(path)
+  assert(path, 'path required')
+  assert(vim.fn.filereadable(path) == 1, ('file not readable: %s'):format(path))
+
+  ---@type { [string]: path }
+  local workspaces = {}
+  local i = 0
+  for line in io.lines(path) do
+    i = i + 1
+    if not (line:match('^#') or line:match('^%s*$')) then
+      local s, e = line:find('%s+')
+      assert(s and s > 1, ('parse error: line %d: %s\n'):format(i, line))
+
+      local name = line:sub(1, s-1)
+      local dir = line:sub(e+1)
+      assert(not workspaces[name],
+        ('duplicate workspace: %s on line %d'):format(name, i))
+
+      ---@type path
+      local _dir = vim.fn.expand(dir)
+      assert(vim.fn.isdirectory(_dir) == 1,
+        ('fs error line %d: directory not found: '):format(i, _dir))
+
+      workspaces[name] = _dir
+    end
+  end
+
+  return workspaces
+end
+
 return {
   'nvim-neorg/neorg',
   lazy = false,
@@ -9,8 +44,10 @@ return {
       ['core.itero'] = {},
       ['core.dirman'] = {
         config = {
-          workspaces = {
-            nvim = "~/.config/nvim",
+          workspaces = read_workspaces(
+            ('%s/workspaces.txt'):format(vim.fn.stdpath('config'))
+          ) or {},
+          default_workspace = "nvim",
           },
         },
       },
