996003c79b693f70dc83742babbccf64c532eb42
Miles Edland <miles.edland@gmail.com>
2024-12-08 09:34:28 -0800

    plugins/hurl.lua - update mappings

--- lua/plugins/hurl.lua
+++ lua/plugins/hurl.lua
@@ -1,37 +1,38 @@
 local function setmaps()
-  local function map(mode, lhs, rhs, desc)
-    vim.api.nvim_buf_set_keymap(0, mode, lhs, rhs, { desc = desc })
-  end
+  local mappings = require('mappings')
+  mappings.nmap({
+    -- execute code
+    { "Hurl: execute buffer", "<F5>", "<cmd>%HurlRunner<cr>" },
+    { "Hurl: execute buffer", "<localleader>eb", "<cmd>%HurlRunner<cr>" },
+    { "Hurl: [e]xecute [f]ile", "<localleader>ef", "<cmd>HurlRunner<cr>" },
+    { "Hurl: [e]xecute [r]equest", "<localleader>er", "vip<esc>:'<,'>HurlRunner" },
+    { "Hurl: [e]xecute up [t]o request", "<localleader>et",
+      function ()
+        vim.cmd.normal('vip')
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), 'itx', false)
 
-  local function nmap(lhs, rhs, desc)
-    map("n", lhs, rhs, desc)
-  end
+        vim.api.nvim_buf_set_mark(0, '<', 1, 0, {})
+        local endline = vim.api.nvim_buf_get_mark(0, '>')[1]
 
-  local function vmap(lhs, rhs, desc)
-    map("v", lhs, rhs, desc)
-  end
+        vim.api.nvim_cmd({ cmd = 'HurlRunner', range = {1, endline} }, {})
+      end
+    },
+    { "Hurl: show last response", "<localleader>e<cr>", "<cmd>HurlShowLastResponse<cr>" },
+    -- environment variables
+    { "Hurl: Set [e]nvironment variable", "<localleader>he", ":HurlSetVariable " },
+    -- NOTE: for now this buffer is only for viewing variables, but modifying is on the roadmap
+    { "Hurl: View environment [V]ariables", "<localleader>hV", "<cmd>HurlManageVariable<cr>" },
+    -- NOTE: can be comma-separated list
+    { "Hurl: Set environment [f]ile", "<localleader>hf", ":HurlSetEnvFile " },
+    -- modes
+    { "Hurl: Toggle popup/[s]plit mode", "<localleader>hs", "<cmd>HurlToggleMode<cr>" },
+    { "Hurl: Toggle [v]erbose mode", "<localleader>hv", "<cmd>HurlToggleMode<cr>" },
+  }, true)
 
-  -- execute code
-  nmap("<F5>", "<cmd>HurlRunner<cr>", "Hurl: execute file")
-  nmap("<localleader>ef", "<cmd>HurlRunner<cr>", "Hurl: [e]xecute [f]ile")
-  nmap("<localleader>er", "<cmd>HurlRunnerAt<cr>", "Hurl: [e]xecute [r]equest")
-  nmap("<localleader>et", "<cmd>HurlRunnerToEntry<cr>", "Hurl: [e]xecute up [t]o request")
-  nmap("<localleader>e<cr>", "<cmd>HurlShowLastResponse<cr>", "Hurl: show last response")
-
-  -- environment variables
-  nmap("<localleader>he", ":HurlSetVariable ", "Hurl: Set [e]nvironment variable")
-  -- NOTE: for now this buffer is only for viewing variables, but modifying is on the roadmap
-  nmap("<localleader>hV", "<cmd>HurlManageVariable<cr>", "Hurl: View environment [V]ariables")
-  -- NOTE: can be comma-separated list
-  nmap("<localleader>hf", ":HurlSetEnvFile ", "Hurl: Set environment [f]ile")
-
-  -- modes
-  nmap("<localleader>hs", "<cmd>HurlToggleMode<cr>", "Hurl: Toggle popup/[s]plit mode")
-  nmap("<localleader>hv", "<cmd>HurlToggleMode<cr>", "Hurl: Toggle [v]erbose mode")
-
-  -- Visual Mappings
-  vmap("<F5>", ":HurlRunner<cr>", "Hurl: Run selection")
-  vmap("<localleader>e", ":HurlRunner<cr>", "Hurl: Run selection")
+  mappings.xmap({
+    { "Hurl: Run selection", "<F5>", ":HurlRunner<cr>" },
+    { "Hurl: Run selection", "<localleader>e", ":HurlRunner<cr>" },
+  }, true)
 end
 
 return {
@@ -59,11 +60,12 @@ return {
       },
     },
   },
-  config = function (opts)
-    require('hurl').setup(opts)
-
+  init = function()
     local groupid = vim.api.nvim_create_augroup("HURL", { clear = true })
     vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, { pattern = "*.hurl", group = groupid, callback = setmaps })
     vim.api.nvim_create_autocmd("FileType", { pattern = "hurl", group = groupid, callback = setmaps })
+  end,
+  config = function (opts)
+    require('hurl').setup(opts)
   end,
 }
