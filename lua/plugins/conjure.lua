51c11b8987237e23826cb0831f793ca9f4d0c053
Miles Edland <miles.edland@gmail.com>
2025-01-19 10:04:45 -0800

    add plugins/conjure.lua

---
+++ lua/plugins/conjure.lua
@@ -0,0 +1,26 @@
+-- ensure Aniseed and Conjure are installed and initialized first so that the
+-- rest of the configuration can be in Fennel
+return {
+  {
+    "Olical/conjure",
+    event = 'VeryLazy',
+    dependencies = {
+      'Olical/aniseed',
+    },
+    init = function()
+      vim.g["conjure#filetypes"] = {
+        "clojure",
+        "fennel",
+        "janet",
+        "lisp",
+        "lua",
+        "python",
+        "racket",
+        "rust",
+        "scheme",
+        -- "sql",
+      }
+      vim.g["conjure#mapping#doc_word"] = false
+    end,
+  },
+}
