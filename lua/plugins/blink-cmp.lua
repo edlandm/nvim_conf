05078a944594713683380184fdaf50b60b08933b
Miles Edland <miles.edland@gmail.com>
2025-02-24 14:08:46 -0800

    plugins/blink-cmp.lua - fix commandline complation following breaking update

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -150,12 +150,42 @@ return {
           should_show_items = function(ctx)
             return ctx.trigger.initial_character == '@'
           end,
+          score_offset = 4,
           opts = {
             cmp_name = 'cmp_rolodex',
           },
         },
+      }
+    },
+
+    cmdline = {
+      enabled = true,
+      keymap = nil, -- Inherits from top level `keymap` config when not set
+      sources = function()
+        local type = vim.fn.getcmdtype()
+        -- Search forward and backward
+        if type == '/' or type == '?' then return { 'buffer' } end
+        -- Commands
+        if type == '@' then return { 'cmdline' } end
+        if type == ':' then
+          if not vim.fn.getcmdline():match("^[%%0-9,'<>%-]*!") then
+            return { 'cmdline' }
+          end
+        end
+        return {}
+      end,
+      completion = {
+        trigger = {
+          show_on_blocked_trigger_characters = {},
+          show_on_x_blocked_trigger_characters = nil, -- Inherits from top level `completion.trigger.show_on_blocked_trigger_characters` config when not set
+        },
+        menu = {
+          auto_show = nil, -- Inherits from top level `completion.menu.auto_show` config when not set
+          draw = {
+            columns = { { 'label', 'label_description', gap = 1 } },
+          },
+        }
       },
-      cmdline = { 'path' }
     },
 
     fuzzy = {
