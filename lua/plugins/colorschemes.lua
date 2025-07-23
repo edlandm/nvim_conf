51b9dd19541250ee50552be730ac431005264535
Miles Edland <miles.edland@gmail.com>
2025-07-23 16:14:27 -0700

    colorschemes(everforest): set high ui_contrast

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,14 +1,14 @@
 return {
   {
-    "neanias/everforest-nvim",
+    'neanias/everforest-nvim',
     version = false,
     lazy = false,
     priority = 1000, -- make sure to load this before all the other start plugins
     -- Optional; default configuration will be used if setup isn't called.
-    main = "everforest",
+    main = 'everforest',
     opts = {
       italics = true,
-      ui_contrast = "high",
+      ui_contrast = 'high',
       dim_inactive_windows = true,
     },
     config = true,
