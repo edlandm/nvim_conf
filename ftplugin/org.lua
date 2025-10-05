496c59673362da80ef7d5cd2faee0fdb83ae352f
Miles Edland <miles.edland@gmail.com>
2025-10-04 21:52:31 -0700

    ftplugin/org.lua - add mapping+command to select codeblock content

--- ftplugin/org.lua
+++ ftplugin/org.lua
@@ -2,6 +2,52 @@ vim.wo.conceallevel = 2
 vim.wo.wrap = false
 vim.wo.concealcursor = 'nc'
 
+local function select_code_block_contents()
+  local ts = vim.treesitter
+  local parser = ts.get_parser(0, "org")
+  local root = parser:parse()[1]:root()
+
+  -- Walk up the tree to find if we're inside a code block
+  local current = root:named_descendant_for_range(
+    vim.api.nvim_win_get_cursor(0)[1] - 1,
+    0,
+    vim.api.nvim_win_get_cursor(0)[1] - 1,
+    0)
+
+  local code
+  while current do
+    if current:type() == "block" then
+      local contents = (current:field("contents") or {})[1]
+      if contents and contents:child_count() > 0 then
+        code = contents
+        break
+      end
+    end
+    current = current:parent()
+  end
+
+  if not code then
+    vim.notify("Cursor is not inside a code block", vim.log.levels.INFO, {})
+    return
+  end
+
+  local content_start_row = code:start()
+  local content_end_row = code:end_()
+
+  local cur_lnum = vim.fn.line('.') - 1
+  local is_cursor_inside_contents = content_start_row < content_end_row
+    and cur_lnum >= content_start_row - 1
+    and cur_lnum <= content_end_row
+
+  if is_cursor_inside_contents then
+    local start_line = content_start_row + 1
+    local end_line = content_end_row
+    vim.api.nvim_win_set_cursor(0, {start_line, 0})
+    vim.cmd("normal! V" .. (end_line - start_line) .. "jo")
+    return
+  end
+end
+
 ---- MAPPINGS ================================================================
 local mappings = require 'config.mappings'
 local lleader = mappings.lleader
@@ -26,6 +72,7 @@ mappings.map {
       end
     },
     { 'Pick: Headlines', 'gs', org.pickers.headlines },
+    { 'Select Codeblock Content', '<leader>v', select_code_block_contents },
     { '', '', '' },
   },
   { mode = 'i', buffer = true,
@@ -45,3 +92,9 @@ mappings.map {
 vim.api.nvim_buf_create_user_command(0, 'OrgLinkToPWD', org.fn.link_to_pwd, {
   desc = 'Link file to :PWD:/index.org',
 })
+
+vim.api.nvim_buf_create_user_command(0, 'OrgSelectCodeBlockContents',
+  select_code_block_contents, {
+    desc = 'Selects the contents of the current Org mode code block',
+  }
+)
